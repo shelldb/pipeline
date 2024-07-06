a1fdf02eb739ff69bf3c29c98ae62825ff7286cd
Vaib <vaibhavkhandalkar1118@gmail.com>
2024-07-06 11:51:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,27 @@ resource "azurerm_resource_group" "test_rg1" {
   name     = "my-resource-group"
   location = "Central US"
 }
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate"
+  location = "Central US"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = first_state
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
